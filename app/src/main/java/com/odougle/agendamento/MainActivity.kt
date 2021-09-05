b26c7a13c98d601fdd78aff1c321888f4fc97b3e
douguizilla <douglasgommez@gmail.com>
2021-09-05 19:22:21 -0300

    set btnOneTime click action

--- app/src/main/java/com/odougle/agendamento/MainActivity.kt
+++ app/src/main/java/com/odougle/agendamento/MainActivity.kt
@@ -2,7 +2,11 @@ package com.odougle.agendamento
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.odougle.agendamento.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +14,25 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val wm = WorkManager.getInstance(this)
+    private var workId: UUID? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btnOneTime.setOnClickListener {
+            val input = Data.Builder()
+                .putString(MyWork.PARAM_FIRST_NAME, "Douglas")
+                .build()
+
+            val request = OneTimeWorkRequest.Builder(MyWork::class.java)
+                .setInputData(input)
+                .build()
+
+            observeAndEnqueue(request)
+        }
+
+
     }
 }
